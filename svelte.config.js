7d1ce3c464033533863dc6d62a8a737aac27919e
Vince Mis <vincemis610@gmail.com>
2025-12-01 00:11:38 -0600

    fix deploy gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
-// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
 
 export default {
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: adapter({
       fallback: 'index.html'
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/guests-bbs' : ''
+      base: dev ? '' : '/guests-bbs'
+    },
+    prerender: {
+      handleHttpError: 'warn'
     }
   }
 };
